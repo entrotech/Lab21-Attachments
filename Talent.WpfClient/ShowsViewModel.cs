a5c39db9f9c5e168a379e8905b7e92d8a9f885da
John Darragh <darragh@entrotech.net>
2016-12-05 20:47:33 -0800

    Add project files.

---
+++ Talent.WpfClient/ShowsViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.ObjectModel;
+using Talent.DataAccess.Ado;
+using Talent.Domain;
+
+namespace Talent.WpfClient
+{
+    public class ShowsViewModel : DomainViewModel<Show>
+    {
+
+        public ShowsViewModel() : base(new ShowRepository())
+        {
+        }
+
+        public ObservableCollection<MpaaRating> MpaaRatings
+        {
+            get {
+                return new ObservableCollection<MpaaRating>( 
+                    LookupCache.MpaaRatings);
+            }
+        }
+
+        public ObservableCollection<CreditType> CreditTypes
+        {
+            get
+            {
+                return new ObservableCollection<CreditType>(
+                    LookupCache.CreditTypes);
+            }
+        }
+
+        public ObservableCollection<Person> People
+        {
+            get
+            {
+                return new ObservableCollection<Person>(
+                    LookupCache.People);
+            }
+        }
+
+        public ObservableCollection<Genre> Genres
+        {
+            get
+            {
+                return new ObservableCollection<Genre>(
+                    LookupCache.Genres);
+            }
+        }
+
+    }
+}
